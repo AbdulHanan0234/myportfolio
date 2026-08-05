2f8786f15ce3c538aa1af4a9770cfb06bd0cf9ff
Abdul Hanan <ah24620055@gmail.com>
2026-08-05 16:03:04 +0500

    feat: initialize portfolio project with core components, styling, and dependencies

---
+++ src/data/constants.jsx
@@ -0,0 +1,291 @@
+import projectImg from "../assets/images/projectImg.png";
+import KFUEIT from "../assets/images/unnamed.jpg";
+import PGCLogo from "../assets/images/PGCLogo.jpg";
+import NGSSLogo from "../assets/images/NGSSLogo.jpg";
+import project1 from "../assets/images/project1.png";
+import project2 from "../assets/images/project2.png";
+import project3 from "../assets/images/project3.png";
+import project4 from "../assets/images/project4.png";
+import project5 from "../assets/images/project5.png";
+import project6 from "../assets/images/project6.png";
+import lms1 from "../assets/images/lms1.png";
+
+
+export const Bio = {
+  name: "Abdul Hanan",
+  roles: ["Full Stack Developer", "React.js Developer", "HTML/CSS Developer"],
+  description:
+    "I am Abdul Hanan, a Computer Science student at Khwaja Fareed University of Engineering & Information Technology.I am passionate about front-end web development and currently building my skills in React.js, JavaScript, HTML, and CSS. I enjoy creating simple, responsive, and user-friendly web applications.My goal is to gain practical experience through internships and real-world projects where I can grow as a developer and contribute to meaningful work.",
+  github: "https://github.com/AbdulHanan0234",
+  resume: "/",
+  linkedin: "https://www.linkedin.com/in/abdulhanan850/",
+  twitter: "/",
+  insta: "https://www.instagram.com/abdulhanan850/",
+  facebook: "https://www.facebook.com/abdulhanan850/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/ab/Official_CSS_Logo.svg",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  // {
+  //   title: "Backend",
+  //   skills: [
+  //     {
+  //       name: "Node Js",
+  //       image: "https://nodejs.org/static/images/logo.svg",
+  //     },
+  //     {
+  //       name: "MongoDB",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+  //     },
+  //     {
+  //       name: "Firebase",
+  //       image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: "Android",
+  //   skills: [
+  //     {
+  //       name: "Java",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+  //     },
+  //     {
+  //       name: "XML",
+  //       image:
+  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+  //     },
+  //     {
+  //       name: "Android Studio",
+  //       image:
+  //         "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+  //     },
+  //   ],
+  // },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: projectImg,
+    role: "I have a done few courses online and offline and built a few projects  ",
+    company: "Flipr Inovations Pvt. Ltd.",
+    date: "jULY 2024 - PRESENT",
+    desc: "I have hands-on experience with these skills and have built a few projects using them. I am always eager to learn new technologies and improve my skills.",
+    skills: [
+      "AWS",
+      "Elastic Beanstalk",
+      "React.js",
+      "HTML",
+      "CSS",
+      "BOOTSTRAP",
+    ],
+    doc: "",
+  },
+  // {
+  //   id: 4,
+  //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+  //   role: "Android Developer Intern",
+  //   company: "Rudraksha Welffare Foundation",
+  //   date: "June 2021 - Oct 2021",
+  //   desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+  //   skills: [
+  //     "Android",
+  //     "Java",
+  //     "Kotlin",
+  //     "XML",
+  //     "Node Js",
+  //     "Cloud Firestore",
+  //     "Firebase",
+  //     "Figma",
+  //   ],
+  //   doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+  // },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: KFUEIT,
+    school:
+      "Khwaja Fareed University of Engineering and Information Technology, Rahim Yar Khan",
+    date: "july 2024 - july 2028",
+    grade: "3.17 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science at Khwaja Fareed University of Engineering and Information Technology, Rahim Yar Khan. I have completed 4 semesters and have a CGPA of 3.17.",
+    degree: "Bachelor of Computer Science",
+  },
+  {
+    id: 1,
+    img: PGCLogo,
+    school: "Punjab Group of Colleges, Rahim Yar Khan",
+    date: "Apr 2022 - June 2024",
+    grade: "67.91%",
+    desc: "I completed my Higher Secondary School (12th)  (Intermediate) at PGC, Rahim Yar Khan, where I studied ICS (Computer Science).",
+    degree: "ISC(12th), Computer Science",
+  },
+  {
+    id: 2,
+    img: NGSSLogo,
+    school: "National Garrison Secondary School, Rahim Yar Khan",
+    date: "Apr 2020 - Apr 2022",
+    grade: "92.3%",
+    desc: "I completed my Scondary School (10th) education at National Garrison Secondary School, Rahim Yar Khan, I did my Metric(10th) in Science.",
+    degree: "HSC(10th), Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 6,
+    title: "My University LMS",
+    date: "JUly 2026",
+    description:
+      "This project is the copy of the Learning Management System (LMS) of my university",
+    image: lms1,
+    tags: ["React Js", "HTML", "CSS","Bootstrap 5","Git"],
+    category: "Web App",
+    github: "https://github.com/AbdulHanan0234/lms",
+    webapp: "https://abdulhanan0234.github.io/lms/",
+  },
+  {
+    id: 5,
+    title: "Ecomerce Website",
+    date: "Jun 2026",
+    description:
+      "I did this project from Youtube, Its only for Windows, Its not responsive",
+    image: project5,
+    tags: ["React Js", "HTML", "CSS"],
+    category: "Web App",
+    github: "https://github.com/AbdulHanan0234/shopper",
+    webapp: "https://shopper-5deheh86l-ah24.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Ecomerce Website",
+    date: "Jun 2026",
+    description:
+      "This project I made using React Js, HTML, CSS for the Frontend and the backend was provided in the course. I did this project from watching a course on YouTube.I deployed my project on AWS(Elastic Beanstalk).",
+    image: project1,
+    tags: ["AWS", "React Js", "HTML", "CSS"],
+    category: "Web App",
+    github: "https://github.com/AbdulHanan0234/ssdev-ecommerce-project",
+    webapp:
+      "http://ecommerce-project-react-env.eba-nmzibzs2.eu-north-1.elasticbeanstalk.com/",
+  },
+  {
+    id: 3,
+    title: "ChatBot",
+    date: "May 2026",
+    description:
+      "this project is a chatbot that I built using React.Js, CSS, HTML for frontend and the backend was provided in the course. I did this project from watching a course on YouTube.I deployed my project on Github Pages.",
+    image: project2,
+    tags: ["React Js", "HTML", "CSS", "GitHub Pages"],
+    category: "web app",
+    github: "https://github.com/AbdulHanan0234/chatbot-project",
+    webapp: "https://abdulhanan0234.github.io/chatbot-project/",
+  },
+
+  {
+    id: 2,
+    title: "Newslettter Website",
+    date: "2025",
+    description:
+      "This is a simple newsletter website that I built using HTML and CSS. It features a clean and modern design with a focus on user experience.",
+    image: project4,
+    tags: ["HTML", "CSS", "GitHub Pages"],
+    category: "web app",
+    github: "https://github.com/AbdulHanan0234/iCoder",
+    webapp: "https://abdulhanan0234.github.io/iCoder/",
+    // member: [
+    //   {
+    //     name: "Rishav Chanda",
+    //     img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+    //     linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    //     github: "https://github.com/rishavchanda/",
+    //   },
+    //   {
+    //     name: "Upasana Chaudhuri",
+    //     img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+    //     linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+    //     github: "https://github.com/upasana0710",
+    //   },
+    // ],
+  },
+  {
+    id: 1,
+    title: "YouTube Clone",
+    date: "2024",
+    description:
+      "This project is a YouTube clone that I built using HTML, CSS.This a single webpage project that I did from watching a course on YouTube. I deployed my project on Github Pages.",
+    image: project3,
+    tags: ["HTML", "CSS", "GitHub Pages"],
+    category: "web app",
+    github: "https://github.com/AbdulHanan0234/AHTube",
+    webapp: "https://abdulhanan0234.github.io/AHTube/",
+  },
+  {
+    id: 0,
+    title: "Ecomerce Website",
+    date: "2024",
+    description:
+      "This project is an e-commerce website that I built using HTML and CSS. It features a clean and modern design with a focus on user experience. I deployed my project on Github Pages.",
+    image: project6,
+    tags: ["HTML", "CSS", "GitHub Pages"],
+    category: "Web App",
+    github: "https://github.com/AbdulHanan0234/RedStore",
+    webapp: "https://abdulhanan0234.github.io/RedStore/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
